a1c7a86881414e9643784f4e97b96fc751ccca9a
Dejan Kutle <kronikpillow@gmail.com>
2022-06-23 09:53:50 +0200

    use kronikpillow voidrice fork

--- larbs.sh
+++ larbs.sh
@@ -14,8 +14,7 @@ while getopts ":a:r:b:p:h" o; do case "${o}" in
 	*) printf "Invalid option: -%s\\n" "$OPTARG" && exit 1 ;;
 esac done
 
-[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
-[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/progs.csv"
+[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/kronikpillow/voidrice.git"
 [ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/kronikpillow/LARBS/master/progs.csv"
 [ -z "$aurhelper" ] && aurhelper="yay"
 [ -z "$repobranch" ] && repobranch="master"
