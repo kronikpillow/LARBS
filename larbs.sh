5345fe060baa3ae1333a823927e470fdaf10b269
Dejan Kutle <kronikpillow@gmail.com>
2022-06-23 03:34:44 +0200

    we use pipewire now

--- larbs.sh
+++ larbs.sh
@@ -243,9 +243,6 @@ sudo -u "$name" mkdir -p "/home/$name/.config/mpd/playlists/"
 grep -q "OTHER_OPTS='-a pulseaudio -m alsa_seq -r 48000'" /etc/conf.d/fluidsynth ||
 	echo "OTHER_OPTS='-a pulseaudio -m alsa_seq -r 48000'" >> /etc/conf.d/fluidsynth
 
-# Start/restart PulseAudio.
-pkill -15 -x 'pulseaudio'; sudo -u "$name" pulseaudio --start
-
 # This line, overwriting the `newperms` command above will allow the user to run
 # serveral important commands, `shutdown`, `reboot`, updating, etc. without a password.
 newperms "%wheel ALL=(ALL) ALL #LARBS
