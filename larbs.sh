3da9851aebb08a9af04051278203eacbc374930e
Dejan Kutle <kronikpillow@gmail.com>
2022-06-24 15:00:02 +0200

    only wheel

--- larbs.sh
+++ larbs.sh
@@ -58,7 +58,7 @@ adduserandpass() { \
 	# Adds user `$name` with password $pass1.
 	dialog --infobox "Adding user \"$name\"..." 4 50
 	useradd -m -g wheel -s /bin/zsh "$name" >/dev/null 2>&1 ||
-	usermod -a -G audio,input,kvm,storage,video,network,log,rfkill,sys,uucp,wheel,dbus,lp,mail "$name" && mkdir -p /home/"$name" && chown "$name":wheel /home/"$name"
+	usermod -a -G wheel "$name" && mkdir -p /home/"$name" && chown "$name":wheel /home/"$name"
 	export repodir="/home/$name/.local/src"; mkdir -p "$repodir"; chown -R "$name":wheel "$(dirname "$repodir")"
 	echo "$name:$pass1" | chpasswd
 	unset pass1 pass2 ;}
